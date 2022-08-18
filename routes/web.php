ae7c0dc11cbd83cfd99b40002b137a3d454d084f
nagasakikoutaro <kmny03@gmail.com>
2022-08-18 10:33:38 +0900

    ログインしないとページを見れないように変更

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('login');;
 Route::get('/create','App\Http\Controllers\BodyTempeatureController@create');
 Route::post('/post','App\Http\Controllers\BodyTempeatureController@post');
-Route::get('/index','App\Http\Controllers\BodyTempeatureController@index');
+Route::get('/index','App\Http\Controllers\BodyTempeatureController@index')->middleware('auth');
